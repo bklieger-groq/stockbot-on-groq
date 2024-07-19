297e6c94cbd4ff32d66b9062c6dd057ce83c55fb
Benjamin Klieger <bklieger@groq.com>
2024-07-18 20:24:44 -0700

    Randomize order of examples on homepage chat

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-
+import { useState, useEffect } from 'react';
 import { Button } from '@/components/ui/button'
 import { PromptForm } from '@/components/prompt-form'
 import { ButtonScrollToBottom } from '@/components/button-scroll-to-bottom'
@@ -37,7 +37,7 @@ export function ChatPanel({
     {
       heading: 'What is the price',
       subheading: 'of Apple Inc.?',
-      message: `What is the price of Apple stock?`
+      message: 'What is the price of Apple stock?'
     },
     {
         heading: 'Show me a stock chart',
@@ -66,6 +66,20 @@ export function ChatPanel({
     },
   ]
 
+  interface ExampleMessage {
+    heading: string;
+    subheading: string;
+    message: string;
+  }
+  
+  const [randExamples, setRandExamples] = useState<ExampleMessage[]>([]);
+  
+  useEffect(() => {
+    const shuffledExamples = [...exampleMessages].sort(() => 0.5 - Math.random());
+    setRandExamples(shuffledExamples);
+  }, []);
+
+
   return (
     <div className="fixed inset-x-0 bottom-0 w-full bg-gradient-to-b from-muted/30 from-0% to-muted/30 to-50% duration-300 ease-in-out animate-in dark:from-background/10 dark:from-10% dark:to-background/80 peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
       <ButtonScrollToBottom
@@ -76,7 +90,7 @@ export function ChatPanel({
       <div className="mx-auto sm:max-w-2xl sm:px-4">
         <div className="mb-4 grid grid-cols-2 gap-2 px-4 sm:px-0">
           {messages.length === 0 &&
-            exampleMessages.map((example, index) => (
+            randExamples.map((example, index) => (
               <div
                 key={example.heading}
                 className={`
